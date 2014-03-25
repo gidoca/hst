ba826f798000009005d01e9278404c78f7f90a03
Gian Calgeer <gidoca@gmail.com>
2014-03-25 14:56:52 +0100

    Only consider points inside Switzerland

---
+++ README.md
@@ -0,0 +1,1 @@
+hst.m calculates the point in Switzerland that is the farthest from a public transport stop. It is free software; see LICENCE.md for details. 
--- hst.m
+++ hst.m
@@ -4,9 +4,20 @@ t = delaunay(o(:, 1), o(:, 2));
 TR = TriRep(t, o(:, 1), o(:, 2));
 [CC, R] = circumcenters(TR);
 [~, sortidx] = sort(R, 'descend');
+
+boundarydata = csvread('swissboundary.csv');
+boundary = [boundarydata(1:840, :); boundarydata(26935:28690, :); boundarydata(841:26094, :)];
+[boundarychx, boundarychy] = deg2ch1903plus(boundary(:, 2), boundary(:, 1));
+
+
 figure; hold on;
 triplot(t, o(:, 1), o(:, 2));
+plot(boundarychx, boundarychy, 'g');
 for i = 1:1000
     in = sortidx(i);
-    plot(CC(in, 1), CC(in, 2), 'o');
+    if inpolygon(CC(in, 1), CC(in, 2), boundarychx, boundarychy)
+        plot(CC(in, 1), CC(in, 2), 'r*');
+        disp(['x = ' int2str(CC(in, 1)) ', y = ' int2str(CC(in, 2))]);
+        break;
+    end
 end
